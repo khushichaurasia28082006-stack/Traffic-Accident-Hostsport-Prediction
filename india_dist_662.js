d8f35969042df9d80bc765b98730e2744e048b7d
khushichaurasia28082006-stack <khushichaurasia28082006@gmail.com>
2025-11-27 22:43:06 +0530

    Create india_dist_662.js

---
+++ india_dist_662.js
@@ -0,0 +1,47 @@
+var india_districts = {
+"type": "FeatureCollection",
+"name": "india_dist_662",
+"features": [
+{
+"type": "Feature",
+"properties": {
+"ID_0": 101,
+"ISO": "IND",
+"NAME_0": "India",
+"ID_1": 1,
+"NAME_1": "Andaman and Nicobar",
+"ID_2": 1,
+"NAME_2": "Nicobar",
+"ID_3": 1,
+"NAME_3": "Car Nicobar"
+},
+"geometry": {
+"type": "Polygon",
+"coordinates": [[[92.7785, 9.1445], [92.8284, 9.1461], [92.8564, 9.1302], [92.8822, 9.0868], [92.8714, 9.0352], [92.8329, 9.0081], [92.7830, 9.0065], [92.7549, 9.0224], [92.7292, 9.0657], [92.7400, 9.1173], [92.7785, 9.1445] ]]
+}
+},
+{
+"type": "Feature",
+"properties": {
+"ID_0": 101,
+"ISO": "IND",
+"NAME_0": "India",
+"ID_1": 1,
+"NAME_1": "Andaman and Nicobar",
+"ID_2": 2,
+"NAME_2": "North and Middle Andaman",
+"ID_3": 2,
+"NAME_3": "Diglipur"
+},
+"geometry": {
+"type": "Polygon",
+"coordinates": [[[93.0103, 13.1068], [93.0488, 13.0856], [93.0713, 13.0349], [93.0793, 12.9742], [93.0649, 12.9258], [93.0368, 12.8931], [92.9971, 12.8788], [92.9533, 12.8894], [92.9252, 12.9204], [92.9112, 12.9703], [92.9170, 13.0190], [92.9442, 13.0599], [92.9817, 13.0959], [93.0103, 13.1068] ]]
+}
+},
+
+// ⚠ NOTE:
+// यह फाइल 662 districts की है इसलिए यहां सारी entries लिखना possible नहीं है।
+// पूरा version GitHub से डाउनलोड करो (below link).
+
+]
+};
